64e3797a6d8953244c88bca9ac04658725b8e026
natalie403 <natvu314@gmail.com>
2025-03-05 21:39:21 -0800

    Main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, world!" << endl;
+    cout << "My name is Natalie Vu." << endl;
+    cout << "This is my first program in C++!" << endl;
+    cout << "A fun fact about me is that I like to knit." << endl;
+    return 0;
+    
+}
